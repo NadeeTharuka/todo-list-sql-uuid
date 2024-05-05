9710e27f22e79b0247f077d77c514abc3a6e8392
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-05-05 14:22:25 +0530

    Login page & mechanism setup & CRUD

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.encoders import jsonable_encoder
 from db import SessionLocal, engine, DBContext
+import models, crud, schemas
 from sqlalchemy.orm import Session
-
+from fastapi_login import LoginManager
+from dotenv import load_dotenv
+import os
 app = FastAPI()
+
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+ACCESS_TOKEN_EXPIRE_MINUTES=60
+
+manager = LoginManager(SECRET_KEY, token_url="/login", use_cookie=True)
+manager.cookie_name = "auth"
+
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -23,4 +35,9 @@ def get_tasks(request: Request, db: Session = Depends(get_db), user: schemas.Use
     "title": "Tasks", 
     "user": user, 
     "tasks": crud.get_tasks_by_user_id(db=db,id=user.id)})
+
+@app.get("/login")
+def get_login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request, "title": "Login"})
+
 
